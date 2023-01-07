f5d0b6752b1d7a29d0d86da79abd1244a7996868
RMF <reksamfitrananda@gmail.com>
2023-01-07 17:57:48 +0700

    final

---
+++ app/src/main/java/com/rmf/mynavigation/FIrstScreen.kt
@@ -0,0 +1,34 @@
+package com.rmf.mynavigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.rmf.mynavigation.destinations.SecondScreenDestination
+
+@RootNavGraph(start = true)
+@Destination
+@Composable
+fun FirstScreen(
+    navigator: DestinationsNavigator
+) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "First Screen")
+        Button(
+            onClick = { navigator.navigate(SecondScreenDestination) },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(24.dp)
+        ) {
+            Text(text = "Navigate to Second Screen")
+        }
+    }
+}
